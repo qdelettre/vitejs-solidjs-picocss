a41184c3fff67f8dd91980a62fa48c67072ba509
qdelettre <quentin.delettre@gmail.com>
2022-01-28 23:52:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing [vite](https://vitejs.dev/)+[solidjs](https://www.solidjs.com/)+[picocss](https://picocss.com/)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+import './App.scss';
+
+const App: Component = () => {
+  return (
+    <main class="container">
+      <h1>Hello, world!</h1>
+    </main>
+  );
+};
+
+export default App;
