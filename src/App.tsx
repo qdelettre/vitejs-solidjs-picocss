1e76565ac154f0083502b395197e332160c997f8
Quentin Delettre <quentin.delettre@gmail.com>
2023-02-04 11:49:28 +0000

    chore: eslint, prettier & husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import type { Component } from 'solid-js';
 import './App.scss';
 
 const App: Component = () => {
-  return (
-    <main class="container">
-      <h1>Hello, world!</h1>
-    </main>
-  );
+    return (
+        <main class="container">
+            <h1>Hello, world!</h1>
+        </main>
+    );
 };
 
 export default App;
